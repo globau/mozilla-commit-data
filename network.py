b8de5e55939169c232d52c2ccc27cfde8ee26a72
Mark Côté <mcote@mozilla.com>
2018-03-17 22:01:29 -0400

    Support POST requests with data.

--- network.py
+++ network.py
@@ -6,6 +6,7 @@
 import json
 import os
 import sys
+from urllib.parse import urlencode
 from urllib.request import urlopen
 
 cache_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'cache')
@@ -13,7 +14,10 @@ if not os.path.exists(cache_path):
     os.mkdir(cache_path)
 
 
-def http_get(url, name, is_json=True):
+def http_get(url, name, is_json=True, data=None):
+    if data:
+        data = urlencode(data).encode('ascii')
+
     cache_file = os.path.join(cache_path, name)
     if os.path.exists(cache_file):
         with open(cache_file, encoding='utf-8') as f:
@@ -24,9 +28,9 @@ def http_get(url, name, is_json=True):
     with open(cache_file, 'w', encoding='utf-8') as f:
         print(f'fetching {url}', file=sys.stderr)
         if is_json:
-            content = json.load(urlopen(url))
+            content = json.load(urlopen(url, data=data))
             json.dump(content, f, indent=2, sort_keys=True)
         else:
-            content = urlopen(url).read().decode('utf-8')
+            content = urlopen(url, data=data).read().decode('utf-8')
             f.write(content)
         return content
